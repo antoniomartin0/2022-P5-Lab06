b7547961b9e7491e48429e9fbd402a7b5e967d21
Antonio Martin <antonio.martin@vtsd.com>
2022-11-14 13:42:33 -0500

    lab06

--- Lab06vst.java
+++ Lab06vst.java
@@ -32,21 +32,30 @@ public class Lab06vst extends Applet
 
         // Draw Random Squares
 
-            Random Int = new Random(12345);
+            Random Int1 = new Random(12345);
             for (int k = 1; k <=100; k++)
             {
-                int x1 = rndInt.nextInt();
-                int y1 = rndInt.nextInt();
-                int x2 = rndInt.nextInt();
-                int y2 = rndInt.nextInt();
-                g.drawRect(x1,y1,x2,y2);
+                int x = rndInt.nextInt();
+                int y = rndInt.nextInt();
+                int red = rndInt.nextInt(256);
+                int green = rndInt.nextInt(256);
+                int blue = rndInt.nextInt(256);
+                g.setColor(new Color (red,green,blue));
+                g.fillRect(x,y,50,50);
             }
 
         // Draw Random Circles
 
+            Random Int2 = new Random(12345);
+            for (int k = 1; k <= 100; k++)
+            {
+            int x1 = rndInt.nextInt(390);
+            int y1 = rndInt.nextInt(300);
+            int x2 = rndInt.nextInt(390);
+            int y2 = rndInt.nextInt(300);
+            g.drawOval(x1,y1,x2,y2);
 
-
-
+            }
 
         // Draw 3-D Box
 
