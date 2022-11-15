76f5b2a79ac5adae73892bf9b9894100efa55efd
Antonio Martin <antonio.martin@vtsd.com>
2022-11-15 13:12:00 -0500

    lab06

--- Lab06vst.java
+++ Lab06vst.java
@@ -9,59 +9,48 @@ import java.util.*;
 public class Lab06vst extends Applet
 {
 
-    public void paint(Graphics g)
-    {
+    public void paint(Graphics g) {
         // Draw Grid
-        g.drawRect(10,10,780,580);
-        g.drawLine(400,10,400,590);
-        g.drawLine(10,300,790,300);
-
+        g.drawRect(10, 10, 780, 580);
+        g.drawLine(400, 10, 400, 590);
+        g.drawLine(10, 300, 790, 300);
 
 
         // Draw Random Lines
 
         Random rndInt = new Random(12345);
-        for (int k = 1; k <= 100; k++)
-        {
-            int x1 = rndInt.nextInt(390);
-            int y1 = rndInt.nextInt(300);
+        for (int k = 1; k <= 100; k++) {
+            int x1 = rndInt.nextInt(400);
+            int y1 = rndInt.nextInt(230);
             int x2 = rndInt.nextInt(390);
             int y2 = rndInt.nextInt(300);
-            g.drawLine(x1,y1,x2,y2);
+            int red = rndInt.nextInt(256);
+            int green = rndInt.nextInt(256);
+            int blue = rndInt.nextInt(256);
+            g.setColor(new Color(red, green, blue));
+            g.drawLine(x1, y1, x2, y2);
         }
 
         // Draw Random Squares
 
-            Random Int1 = new Random(12345);
-            for (int k = 1; k <=100; k++)
-            {
-                int x = rndInt.nextInt();
-                int y = rndInt.nextInt();
-                int red = rndInt.nextInt(256);
-                int green = rndInt.nextInt(256);
-                int blue = rndInt.nextInt(256);
-                g.setColor(new Color (red,green,blue));
-                g.fillRect(x,y,50,50);
-            }
+        Random Int1 = new Random(12345);
+        for (int k = 1; k <= 100; k++) {
+            int x = rndInt.nextInt(395);
+            int y = rndInt.nextInt(295);
+            int red = rndInt.nextInt(256);
+            int green = rndInt.nextInt(256);
+            int blue = rndInt.nextInt(256);
+            g.setColor(new Color(red, green, blue));
+            g.fillRect(x, y, 50,50);
+        }
 
         // Draw Random Circles
 
-            Random Int2 = new Random(12345);
-            for (int k = 1; k <= 100; k++)
-            {
-            int x1 = rndInt.nextInt(390);
-            int y1 = rndInt.nextInt(300);
-            int x2 = rndInt.nextInt(390);
-            int y2 = rndInt.nextInt(300);
-            g.drawOval(x1,y1,x2,y2);
 
-            }
 
         // Draw 3-D Box
 
-
-
-
+        g.fillRect(470,375,100,100);
     }
 
 }
